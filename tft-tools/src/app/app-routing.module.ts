e6590c3f379a9a4a06c3254922cb5b7e19384c97
bkim287 <brandonkim287@gmail.com>
2023-05-23 22:29:33 -0400

    Updated base app files

--- tft-tools/src/app/app-routing.module.ts
+++ tft-tools/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoutePathEnum } from './shared-module/utilities/enums/route-path.enum';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: RoutePathEnum.Empty, redirectTo: `/${RoutePathEnum.Stats}`, pathMatch: 'full'
+  },
+  {
+    path: RoutePathEnum.Stats, loadChildren: () => import('./modules/stats/stats.module')
+  },
+  {
+    path: RoutePathEnum.Home, loadChildren: () => import('./modules/home/home.module')
+  },
+  {
+    path: RoutePathEnum.Tools, loadChildren: () => import('./modules/tools/tools.module')
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
